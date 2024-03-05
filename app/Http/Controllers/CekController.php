39290e8d45aca681b970b7d95e501e9863c8bdec
saktiworkstation <saktiworkstation@gmail.com>
2024-03-05 18:53:32 +0700

    Try chekking using middlewere

---
+++ app/Http/Controllers/CekController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CekController extends Controller
+{
+    public function index(){
+        return '<h1>Cek2</h1>';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CekController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,5 +28,11 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/cek1', function () {
+    return view('<h1>Cek1</h1>');
+})->middleware(['auth', 'verified']);
+
+Route::get('/cek2', [CekController::class, 'edit'])->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';

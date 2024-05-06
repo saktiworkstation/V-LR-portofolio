0edf40892b802dbc94eb885724e75ff05d8699e1
saktiworkstation <saktiworkstation@gmail.com>
2024-05-06 20:34:09 +0700

    Change table style

--- resources/views/experience/index.blade.php
+++ resources/views/experience/index.blade.php
@@ -9,23 +9,23 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __('Table Experience') }}
-                    <table class="border-collapse border border-slate-500">
-                        <thead>
+                    {{ __('Experience Data') }}
+                    <table class="border-collapse border-slate-500">
+                        <thead class="bg-gray-800 text-white">
                             <tr>
-                                <th class="border border-slate-600">Company</th>
-                                <th class="border border-slate-600">Order</th>
-                                <th class="border border-slate-600">Field</th>
-                                <th class="border border-slate-600">Duration</th>
+                                <th class="px-6 py-2 border border-slate-600">Company</th>
+                                <th class="px-6 py-2 border border-slate-600">Order</th>
+                                <th class="px-6 py-2 border border-slate-600">Field</th>
+                                <th class="px-6 py-2 border border-slate-600">Duration</th>
                             </tr>
                         </thead>
-                        <tbody>
+                        <tbody class="bg-gray-500">
                             @foreach ($datas as $data)
-                                <tr>
-                                    <td class="border border-slate-700">{{ $data->company }}</td>
-                                    <td class="border border-slate-700">{{ $data->order }}</td>
-                                    <td class="border border-slate-700">{{ $data->field }}</td>
-                                    <td class="border border-slate-700">{{ $data->duration }}</td>
+                                <tr class="text-white">
+                                    <td class="px-6 py-2 border border-slate-700">{{ $data->company }}</td>
+                                    <td class="px-6 py-2 border border-slate-700">{{ $data->order }}</td>
+                                    <td class="px-6 py-2 border border-slate-700">{{ $data->field }}</td>
+                                    <td class="px-6 py-2 border border-slate-700">{{ $data->duration }}</td>
                                 </tr>
                             @endforeach
                         </tbody>

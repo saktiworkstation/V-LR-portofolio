91c5630bb068df71ae9b5073e16152dddd76f18d
saktiworkstation <saktiworkstation@gmail.com>
2024-05-07 16:46:13 +0700

    changes class

--- resources/views/experience/index.blade.php
+++ resources/views/experience/index.blade.php
@@ -6,9 +6,9 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
                     <h4 class="pb-3 font-semibold text-xl text-gray-800 leading-tight">
                         {{ __('Experience Data') }}
                     </h4>

2607d9ae56a34f4c0f74f59f45ef288701354dfe
saktiworkstation <saktiworkstation@gmail.com>
2024-05-07 15:45:17 +0700

    create forms according to system requirements

--- resources/views/experience/create.blade.php
+++ resources/views/experience/create.blade.php
@@ -24,39 +24,31 @@
                             @csrf
                         </form>
 
-                        <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+                        <form method="post" action="{{ route('eperience.create') }}" class="mt-6 space-y-6">
                             @csrf
                             <div>
-                                <x-input-label for="name" :value="__('Name')" />
-                                <x-text-input id="name" name="name" type="text" class="mt-1 block w-full"
-                                    :value="old('name', $user->name)" required autofocus autocomplete="name" />
-                                <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                                <x-input-label for="company" :value="__('Company')" />
+                                <x-text-input id="company" name="company" type="text" class="mt-1 block w-full"
+                                    :value="old('company', $user->company)" required autofocus autocomplete="company" />
+                                <x-input-error class="mt-2" :messages="$errors->get('company')" />
                             </div>
-
                             <div>
-                                <x-input-label for="email" :value="__('Email')" />
-                                <x-text-input id="email" name="email" type="email" class="mt-1 block w-full"
-                                    :value="old('email', $user->email)" required autocomplete="username" />
-                                <x-input-error class="mt-2" :messages="$errors->get('email')" />
-
-                                @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && !$user->hasVerifiedEmail())
-                                    <div>
-                                        <p class="text-sm mt-2 text-gray-800">
-                                            {{ __('Your email address is unverified.') }}
-
-                                            <button form="send-verification"
-                                                class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                                {{ __('Click here to re-send the verification email.') }}
-                                            </button>
-                                        </p>
-
-                                        @if (session('status') === 'verification-link-sent')
-                                            <p class="mt-2 font-medium text-sm text-green-600">
-                                                {{ __('A new verification link has been sent to your email address.') }}
-                                            </p>
-                                        @endif
-                                    </div>
-                                @endif
+                                <x-input-label for="order" :value="__('Order')" />
+                                <x-text-input id="order" name="order" type="text" class="mt-1 block w-full"
+                                    :value="old('order', $user->order)" required autofocus autocomplete="order" />
+                                <x-input-error class="mt-2" :messages="$errors->get('order')" />
+                            </div>
+                            <div>
+                                <x-input-label for="field" :value="__('Field')" />
+                                <x-text-input id="field" name="field" type="text" class="mt-1 block w-full"
+                                    :value="old('field', $user->field)" required autofocus autocomplete="field" />
+                                <x-input-error class="mt-2" :messages="$errors->get('field')" />
+                            </div>
+                            <div>
+                                <x-input-label for="duration" :value="__('Year Duration')" />
+                                <x-text-input id="duration" name="duration" type="text" class="mt-1 block w-full"
+                                    :value="old('duration', $user->duration)" required autofocus autocomplete="duration" />
+                                <x-input-error class="mt-2" :messages="$errors->get('duration')" />
                             </div>
 
                             <div class="flex items-center gap-4">

0dea7fbe5a4d99e97d7940df674aedaab7d013d7
saktiworkstation <saktiworkstation@gmail.com>
2024-05-07 17:58:41 +0700

    Link url

--- resources/views/experience/index.blade.php
+++ resources/views/experience/index.blade.php
@@ -9,6 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
+                    <x-link-primary-button :url="route('experience.create')">{{ __('Add New Experience') }}</x-link-primary-button>
+                </div>
+            </div>
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
                     <h4 class="pb-3 font-semibold text-xl text-gray-800 leading-tight">
                         {{ __('Experience Data') }}
                     </h4>

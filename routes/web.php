a39d4723d09bcd050f02df3a11e4e1bd5895d02c
saktiworkstation <saktiworkstation@gmail.com>
2024-05-09 20:34:30 +0700

    Create landing-navigation

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('landing-page');
-});
+})->name('/');
 
 Route::get('/dashboard', function () {
     return view('dashboard');

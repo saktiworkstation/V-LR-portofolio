291b064364cc0926562871c3b5972fd387860700
saktiworkstation <saktiworkstation@gmail.com>
2024-05-09 20:32:58 +0700

    Delete heading

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -18,16 +18,7 @@
 
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100">
-        {{-- @include('layouts.navigation') --}}
-
-        <!-- Page Heading -->
-        @if (isset($header))
-            <header class="bg-white shadow border-b-2">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-        @endif
+        @include('layouts.landing-navigation')
 
         <!-- Page Content -->
         <main>

0954c170985c50f86b2ba5f963d7beb6de1a8f13
saktiworkstation <saktiworkstation@gmail.com>
2024-05-09 17:14:22 +0700

    Change landing page desain

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -1,16 +1,17 @@
-<!doctype html>
-<html>
+<x-landing-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Landing') }}
+        </h2>
+    </x-slot>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./output.css" rel="stylesheet">
-</head>
-
-<body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
-</body>
-
-</html>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __('Landing Page') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-landing-layout>
--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -18,7 +18,7 @@
 
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100">
-        @include('layouts.navigation')
+        {{-- @include('layouts.navigation') --}}
 
         <!-- Page Heading -->
         @if (isset($header))

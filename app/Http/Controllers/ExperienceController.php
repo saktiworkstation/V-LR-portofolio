c30b8b274f8b55e1cb21250f259d1108771b2e25
saktiworkstation <saktiworkstation@gmail.com>
2024-05-11 13:07:57 +0700

    fixed redirectiong  url

--- app/Http/Controllers/ExperienceController.php
+++ app/Http/Controllers/ExperienceController.php
@@ -39,7 +39,7 @@ class ExperienceController extends Controller
 
         Experience::create($validatedData);
 
-        return redirect('experience.create')->with('success', 'New ecperience has been added!');
+        return redirect('experience')->with('success', 'New ecperience has been added!');
     }
 
     /**

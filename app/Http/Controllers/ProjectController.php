a1af8c9f2b8fd06ede94ce7bc580666489810373
saktiworkstation <saktiworkstation@gmail.com>
2024-05-16 09:00:00 +0700

    Command to access the project index page

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -12,7 +12,9 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        return view('project.index',[
+            'datas' => Project::latest()->get(),
+        ]);
     }
 
     /**

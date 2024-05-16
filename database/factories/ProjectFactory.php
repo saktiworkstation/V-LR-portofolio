8e19b9f8bae407c6b027b3112f5fea817237d71e
saktiworkstation <saktiworkstation@gmail.com>
2024-05-16 08:15:37 +0700

    Create Project fatory

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'img' => '',
+            'link' => $this->faker->url,
+        ];
+    }
+}

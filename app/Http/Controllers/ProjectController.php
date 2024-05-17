fd30d3c9f390d8ea811f2870f0beab61a5d01f17
saktiworkstation <saktiworkstation@gmail.com>
2024-05-17 13:42:19 +0700

    maintenence

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -42,14 +42,6 @@ class ProjectController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(Project $project)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      */
     public function edit(Project $project)

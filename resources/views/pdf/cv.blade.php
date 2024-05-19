2cfb15a17fc643d804a058419bca8a74812dd0e5
saktiworkstation <saktiworkstation@gmail.com>
2024-05-19 19:55:12 +0700

    use bootstrap for layouting cv

--- resources/views/pdf/cv.blade.php
+++ resources/views/pdf/cv.blade.php
@@ -1,17 +1,26 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CV_Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    ini cv
-
-    <a href="/pdf/download/cv">Download CV here</a>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 bg-black text-white">
+                <h1>User Name</h1>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
+{{-- <a href="/pdf/download/cv">Download CV here</a> --}}

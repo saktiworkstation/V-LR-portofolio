6010fe742de785d194747dfc64ae4899a7f405e9
saktiworkstation <saktiworkstation@gmail.com>
2024-05-19 11:50:59 +0700

    added a command to access the cv page

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Education;
+use App\Models\Experience;
+use App\Models\Interest;
+use App\Models\Project;
+use App\Models\Skill;
 use Illuminate\Http\Request;
+use PDF;
 
 class PDFController extends Controller
 {
     public function showCV(){
-        //
+        return view('pdf.cv', [
+            'skills' => Skill::latest()->get(),
+            'projects' => Project::latest()->get(),
+            'educations' => Education::latest()->get(),
+            'intereses' => Interest::latest()->get(),
+            'experineces' => Experience::latest()->get(),
+        ]);
     }
 
     public function downloadCV(){

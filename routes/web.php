b9916b369c4524edbb1e5f740483825d6f6db352
saktiworkstation <saktiworkstation@gmail.com>
2024-05-19 16:46:30 +0700

    Maintain route

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,12 @@ Route::get('/', function () {
     return view('landing-page');
 })->name('/');
 
+Route::get('/pdf/show/cv', [PDFController::class, 'showCV'])->name('showcv');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/pdf/show/cv', [PDFController::class, 'showCV'])->name('showcv');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');

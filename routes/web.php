2807a0fc959fd8914d2333c1cde166c32edb4cfb
saktiworkstation <saktiworkstation@gmail.com>
2024-05-25 08:07:33 +0700

    Modify profile route

--- routes/web.php
+++ routes/web.php
@@ -35,9 +35,11 @@ Route::get('/dashboard', function () {
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::controller(ProfileController::class)->group(function () {
+        Route::get('/profile', 'edit')->name('profile.edit');
+        Route::patch('/profile', 'update')->name('profile.update');
+        Route::delete('/profile','destroy')->name('profile.destroy');
+    });
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {

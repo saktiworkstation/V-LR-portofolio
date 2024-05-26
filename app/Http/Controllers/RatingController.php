268f677e9ad4a47976280c9fec02d50905d486d3
saktiworkstation <saktiworkstation@gmail.com>
2024-05-26 23:14:15 +0700

    added command to acces index rating page

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -12,7 +12,9 @@ class RatingController extends Controller
      */
     public function index()
     {
-        //
+        return view('rating.index',[
+            'datas' => Rating::latest()->get(),
+        ]);
     }
 
     /**

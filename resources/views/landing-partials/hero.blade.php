cfc4bc05ada8a2a136ddadecaa312923cfa1aae9
saktiworkstation <saktiworkstation@gmail.com>
2024-05-29 14:59:28 +0700

    Trying fixed hero

--- resources/views/landing-partials/hero.blade.php
+++ resources/views/landing-partials/hero.blade.php
@@ -1,36 +1,40 @@
+{{-- hero start --}}
+<div class="container px-40 py-32 bg-gradient-to-t from-slate-200">
+    <div class="row flex justify-center h-96 items-center">
+        <div class="w-1/2">
+            {{-- Content --}}
+            <h1 class="text-4xl font-bold mb-5 text-slate-700">
+                Learn Without Limits
+            </h1>
+            <p class="text-slate-500">Welcome to the world of limitless learning, where knowledge and skills are the keys
+                to
+                freedom. We proudly
+                present 'E-Learning Beyond Limits.' Here, you are the architect of your own educational journey. There
+                are
+                no boundaries, no restrictions, only boundless potential</p>
 
-
-<!-- Container -->
-<div class="relative flex flex-col items-center mx-auto lg:flex-row-reverse lg:max-w-5xl lg:mt-12 xl:max-w-6xl">
-
-    <!-- Image Column -->
-    <div class="w-full h-auto max-h-2000 max-w-2000 lg:w-1/2 overflow-hidden h-[500px]">
-        <img src="https://images.pexels.com/photos/19177009/pexels-photo-19177009/free-photo-of-pria-laki-laki-lelaki-kacamata-hitam.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="foto model" width="2000" height="2000">
-    </div>
-    <!-- Close Image Column -->
-
-    <!-- Text Column -->
-    <div
-        class="max-w-lg bg-white md:max-w-2xl md:z-10 md:shadow-lg md:absolute md:top-0 md:mt-48 lg:w-3/5 lg:left-0 lg:mt-20 lg:ml-20 xl:mt-24 xl:ml-12">
-        <!-- Text Wrapper -->
-        <div class="flex flex-col p-12 md:px-16">
-            <h2 class="text-2xl font-medium uppercase text-black lg:text-4xl">Helo! My Name Is John L</h2>
-            <p class="mt-4">
-                Frontend Engineer using HTML, CSS, Javascript.
-                Backend using Node, Express, SQL.
-                I can assist in building web, mobile, and desktop application.
-            </p>
-            <!-- Button Container -->
-            <div class="mt-8">
-                <a href="#"
-                    class="inline-block w-full text-center text-lg font-medium text-white bg-black border-solid border-2 border-black-600 py-4 px-10 hover:bg-gray-800 hover:shadow-md md:w-48">Contact Me</a>
+            {{-- Search input --}}
+            <div class="relative mt-7 mb-2">
+                <input type="search" id="default-search"
+                    class="block w-full p-4 text-sm text-gray-900 shadow-lg border-none rounded-lg bg-gray-50"
+                    placeholder="Cari kursus, instruktur, dan organisasi..." required />
+                <button type="submit"
+                    class="text-white absolute end-2.5 bottom-2.5 bg-pink-500 hover:bg-pink-600 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-4 py-2"><svg
+                        class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg></button>
             </div>
+
+            {{-- button --}}
+            <a href="#" rel="noopener noreferrer"
+                class="uppercase inline-flex items-center px-4 py-5 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white tracking-widest hover:bg-gray-700 my-5">Explore
+                all course</a>
         </div>
-        <!-- Close Text Wrapper -->
+        <div class="w-1/2">
+            <img src="/img/hero.png" alt="Be Good" class="w-full">
+        </div>
     </div>
-    <!-- Close Text Column -->
-
 </div>
-
-
-
+{{-- hero end --}}

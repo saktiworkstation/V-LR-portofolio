2e1060516b711381089e302f611cb64fcc442ff5
saktiworkstation <saktiworkstation@gmail.com>
2024-05-29 15:03:15 +0700

    Delete search

--- resources/views/landing-partials/hero.blade.php
+++ resources/views/landing-partials/hero.blade.php
@@ -1,5 +1,5 @@
 {{-- hero start --}}
-<div class="container px-40 py-32 bg-gradient-to-t from-slate-200">
+<div class="container px-40 py-40 bg-gradient-to-t from-slate-200">
     <div class="row flex justify-center h-96 items-center">
         <div class="w-1/2">
             {{-- Content --}}
@@ -13,19 +13,7 @@
                 are
                 no boundaries, no restrictions, only boundless potential</p>
 
-            {{-- Search input --}}
-            <div class="relative mt-7 mb-2">
-                <input type="search" id="default-search"
-                    class="block w-full p-4 text-sm text-gray-900 shadow-lg border-none rounded-lg bg-gray-50"
-                    placeholder="Cari kursus, instruktur, dan organisasi..." required />
-                <button type="submit"
-                    class="text-white absolute end-2.5 bottom-2.5 bg-pink-500 hover:bg-pink-600 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-4 py-2"><svg
-                        class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 20 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                    </svg></button>
-            </div>
+            <div class="py-4"></div>
 
             {{-- button --}}
             <a href="#" rel="noopener noreferrer"

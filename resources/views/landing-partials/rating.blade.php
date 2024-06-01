ff5feef8aaa061debbfce351ab4a9d97a07cd2ac
saktiworkstation <saktiworkstation@gmail.com>
2024-06-01 22:10:39 +0700

    added rating foreace

--- resources/views/landing-partials/rating.blade.php
+++ resources/views/landing-partials/rating.blade.php
@@ -9,100 +9,41 @@
 
         {{-- Card --}}
         <div class="grid-rows-1 flex justify-center h-auto items-center relative">
-            {{-- Newman start --}}
-            <div class="flex justify-center h-auto items-start pt-12 px-4">
-                <div class="w-auto max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow-2xl">
-                    <div class="content-center items-center flex justify-start">
-                        <img src="/img/rating/satu.png" alt=""
-                            class="w-64 bg-gradient-to-br h-40 from-green-600 to-emerald-400 rounded-lg shadow ">
-                    </div>
-                    <div class="my-2 h-auto text-center w-64">
-                        <h1 class="text-lg font-bold tracking-tight text-slate-700">
-                            Ryan Newman
-                        </h1>
-                        <p class="font-bold text-xs text-gray-600">Data Analyst at Microsoft
-                        </p>
-
-                        <p class="font-bold text-xs text-gray-600 pt-6">"We've used Wesclic University for the last 2
-                            years. Thanks for the great service."
-                        </p>
-
-
-                        <div class="space-y-1  my-2">
-                            <p class="font-bold text-xs text-gray-600 items-center flex justify-center content-center">
-                                <img src="/img/rating/fstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
-                                <img src="/img/rating/fstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
-                                <img src="/img/rating/fstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
-                                <img src="/img/rating/fstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
-                                <img src="/img/rating/kstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
+            @foreach ($ratings as $item)
+                {{-- start --}}
+                <div class="flex justify-center h-auto items-start pt-12 px-4">
+                    <div class="w-auto max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow-2xl">
+                        <div class="content-center items-center flex justify-start">
+                            <img src="/img/rating/satu.png" alt=""
+                                class="w-64 bg-gradient-to-br h-40 from-green-600 to-emerald-400 rounded-lg shadow ">
+                        </div>
+                        <div class="my-2 h-auto text-center w-64">
+                            <h1 class="text-lg font-bold tracking-tight text-slate-700">
+                                Ryan Newman
+                            </h1>
+                            <p class="font-bold text-xs text-gray-600">Data Analyst at Microsoft
                             </p>
+
+                            <p class="font-bold text-xs text-gray-600 pt-6">"We've used Wesclic University for the last
+                                2
+                                years. Thanks for the great service."
+                            </p>
+
+
+                            <div class="space-y-1  my-2">
+                                <p
+                                    class="font-bold text-xs text-gray-600 items-center flex justify-center content-center">
+                                    <img src="/img/rating/fstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
+                                    <img src="/img/rating/fstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
+                                    <img src="/img/rating/fstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
+                                    <img src="/img/rating/fstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
+                                    <img src="/img/rating/kstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-            {{-- Megan start --}}
-            <div class="flex justify-center h-auto items-start pt-12 px-4">
-                <div class="w-auto max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow-2xl">
-                    <div class="content-center items-center flex justify-start">
-                        <img src="/img/rating/dua.png" alt=""
-                            class="w-64 bg-gradient-to-br h-40 from-green-600 to-emerald-400 rounded-lg shadow ">
-                    </div>
-                    <div class="my-2 h-auto text-center w-64">
-                        <h1 class="text-lg font-bold tracking-tight text-slate-700">
-                            Megan Hayward
-                        </h1>
-                        <p class="font-bold text-xs text-gray-600">System Administrator at Amazon
-                        </p>
-
-                        <p class="font-bold text-xs text-gray-600 pt-6">"We're loving it. Wesclic University is both
-                            perfect and highly adaptable."
-                        </p>
-
-
-                        <div class="space-y-1  my-2">
-                            <p class="font-bold text-xs text-gray-600 items-center flex justify-center content-center">
-                                <img src="/img/rating/fstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
-                                <img src="/img/rating/fstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
-                                <img src="/img/rating/fstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
-                                <img src="/img/rating/fstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
-                                <img src="/img/rating/kstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            {{-- Hope start --}}
-            <div class="flex justify-center h-auto items-start pt-12 px-4">
-                <div class="w-auto max-w-sm p-3 bg-white border border-gray-200 rounded-lg shadow-2xl">
-                    <div class="content-center items-center flex justify-start">
-                        <img src="/img/rating/tiga.png" alt=""
-                            class="w-64 bg-gradient-to-br h-40 from-green-600 to-emerald-400 rounded-lg shadow ">
-                    </div>
-                    <div class="my-2 h-auto text-center w-64">
-                        <h1 class="text-lg font-bold tracking-tight text-slate-700">
-                            Natasha Hope
-                        </h1>
-                        <p class="font-bold text-xs text-gray-600">IT Technician at IBM
-                        </p>
-
-                        <p class="font-bold text-xs text-gray-600 pt-6">"I am really satisfied with my Wesclic
-                            University. It's the perfect solution for bussiness
-                        </p>
-
-                        <div class="space-y-1  my-2">
-                            <p class="font-bold text-xs text-gray-600 items-center flex justify-center content-center">
-                                <img src="/img/rating/fstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
-                                <img src="/img/rating/fstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
-                                <img src="/img/rating/fstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
-                                <img src="/img/rating/fstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
-                                <img src="/img/rating/kstar.png" alt="" class="w-4 h-4 mt-4 mx-1">
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            @endforeach
 
             {{-- Side button --}}
             <a href="#" rel="noopener noreferrer"

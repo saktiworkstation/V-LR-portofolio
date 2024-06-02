39cf8cf337c45ff4507c175b4de932fc6f9b19e0
saktiworkstation <saktiworkstation@gmail.com>
2024-06-02 10:48:33 +0700

    Foreace data skills Compleated

--- resources/views/landing-partials/skill.blade.php
+++ resources/views/landing-partials/skill.blade.php
@@ -4,68 +4,40 @@
         <!-- To achieve the desired progress, you can update the 'stroke-dasharray' property. -->
         <h4 class="text-3xl md:text-5xl dark:text-black font-bold mb-6">Skills</h4>
 
-        <div class="mb-7">
-            <div class="flex justify-between py-1">
-                <span class="text-base text-gray-lite font-semibold dark:text-[#A6A6A6]">Web Design</span>
-                <span class="text-base font-semibold text-gray-lite pr-5 dark:text-[#A6A6A6]">80%</span>
-            </div>
-            <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
-                <path class="rc-progress-line-trail" d="M 0.5,0.5
+        @foreach ($skils as $item)
+            <div class="mb-7">
+                <div class="flex justify-between py-1">
+                    <span class="text-base text-gray-lite font-semibold dark:text-[#A6A6A6]">{{ $item->name }}</span>
+                    <span class="text-base font-semibold text-gray-lite pr-5 dark:text-[#A6A6A6]">
+                        @if ($item->skill_level == 'Beginner')
+                            25%
+                        @elseif ($item->skill_level == 'Intermediate')
+                            50%
+                        @elseif ($item->skill_level == 'Advanced')
+                            75%
+                        @elseif ($item->skill_level == 'Expert')
+                            100%
+                        @endif
+                    </span>
+                </div>
+                <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
+                    <path class="rc-progress-line-trail" d="M 0.5,0.5
          L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
-                <path class="rc-progress-line-path" d="M 0.5,0.5
+                    <path class="rc-progress-line-path" d="M 0.5,0.5
          L 99.5,0.5" stroke-linecap="round" stroke="#FF6464" stroke-width="1" fill-opacity="0"
-                    style="stroke-dasharray: 79.2px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
-                </path>
-            </svg>
-        </div>
-
-
-        <div class="mb-7">
-            <div class="flex justify-between py-1">
-                <span class="text-base text-gray-lite font-semibold dark:text-[#A6A6A6]">Mobile App </span>
-                <span class="text-base font-semibold text-gray-lite pr-5 dark:text-[#A6A6A6]">95%</span>
+                        style="stroke-dasharray:
+                        @if ($item->skill_level == 'Beginner') 25px
+                        @elseif ($item->skill_level == 'Intermediate')
+                            50px
+                        @elseif ($item->skill_level == 'Advanced')
+                            75px
+                        @elseif ($item->skill_level == 'Expert')
+                            100px @endif
+                        100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
+                    </path>
+                </svg>
             </div>
-            <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
-                <path class="rc-progress-line-trail" d="M 0.5,0.5
-         L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
-                <path class="rc-progress-line-path" d="M 0.5,0.5
-         L 99.5,0.5" stroke-linecap="round" stroke="#9272D4" stroke-width="1" fill-opacity="0"
-                    style="stroke-dasharray: 94.05px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
-                </path>
-            </svg>
-        </div>
-
-
-        <div class="mb-7">
-            <div class="flex justify-between py-1">
-                <span class="text-base text-gray-lite font-semibold dark:text-[#A6A6A6]">Illustrator</span>
-                <span class="text-base font-semibold text-gray-lite pr-5 dark:text-[#A6A6A6]">65%</span>
-            </div>
-            <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
-                <path class="rc-progress-line-trail" d="M 0.5,0.5
-         L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
-                <path class="rc-progress-line-path" d="M 0.5,0.5
-         L 99.5,0.5" stroke-linecap="round" stroke="#5185D4" stroke-width="1" fill-opacity="0"
-                    style="stroke-dasharray: 64.35px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
-                </path>
-            </svg>
-        </div>
-
-
-        <div class="mb-7">
-            <div class="flex justify-between py-1">
-                <span class="text-base text-gray-lite font-semibold dark:text-[#A6A6A6]">Photoshope</span>
-                <span class="text-base font-semibold text-gray-lite pr-5 dark:text-[#A6A6A6]">75%</span>
-            </div>
-            <svg class="rc-progress-line" viewBox="0 0 100 1" preserveAspectRatio="none">
-                <path class="rc-progress-line-trail" d="M 0.5,0.5
-         L 99.5,0.5" stroke-linecap="round" stroke="#D9D9D9" stroke-width="1" fill-opacity="0"></path>
-                <path class="rc-progress-line-path" d="M 0.5,0.5
-         L 99.5,0.5" stroke-linecap="round" stroke="#CA56F2" stroke-width="1" fill-opacity="0"
-                    style="stroke-dasharray: 74.25px, 100px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s linear 0s, 0.06s;">
-                </path>
-            </svg>
-        </div>
+        @endforeach
 
     </div>
 </div>

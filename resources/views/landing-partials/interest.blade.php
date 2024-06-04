65d72e4ccefb3b71c2c5423a0624c78c4b718b7d
saktiworkstation <saktiworkstation@gmail.com>
2024-06-04 21:00:07 +0700

    Start Looping Interest Data

--- resources/views/landing-partials/interest.blade.php
+++ resources/views/landing-partials/interest.blade.php
@@ -19,59 +19,6 @@
             </div>
         </div>
         {{-- card end-1 --}}
-
-        {{-- card start 2 --}}
-        <div class="max-w-sm p-4 items-center text-center">
-            <div class="p-2 bg-gradient-to-br h-28 from-rose-500 to-purple-500 rounded-lg shadow content-center">
-                <h1 class="text-5xl font-bold tracking-tight text-white">
-                    132
-                </h1>
-            </div>
-            <div class="pt-5 max-w-sm">
-                <h1 class="text-2xl font-bold tracking-tight text-slate-700">
-                    Siswa Bahagia
-                </h1>
-                <p class="pt-2 font-normal text-gray-500 dark:text-gray-400 ">Mendafar dalam kursus kami dan
-                    meningkatkan
-                    keterampilan mereka
-                </p>
-            </div>
-        </div>
-        {{-- card end 2 --}}
-
-        {{-- card start 3 --}}
-        <div class="max-w-sm p-4 items-center text-center">
-            <div class="p-2 bg-gradient-to-br h-28 from-emerald-500 to-green-500 rounded-lg shadow content-center">
-                <h1 class="text-5xl font-bold tracking-tight text-white">
-                    12
-                </h1>
-            </div>
-            <div class="pt-5 max-w-sm">
-                <h1 class="text-2xl font-bold tracking-tight text-slate-700">
-                    Kelas langsung
-                </h1>
-                <p class="pt-2 font-normal text-gray-500 dark:text-gray-400 ">Tingkatkan keterampilan anda dengan aliran
-                    pengetahuan langsung
-                </p>
-            </div>
-        </div>
-        {{-- card end 3 --}}
-
-        {{-- card start 4 --}}
-        <div class="max-w-sm p-4 items-center text-center">
-            <div class="p-2 bg-gradient-to-br h-28 from-rose-600 to-orange-300 rounded-lg shadow content-center">
-                <h1 class="text-5xl font-bold tracking-tight text-white">
-                    31
-                </h1>
-            </div>
-            <div class="pt-5 max-w-sm">
-                <h1 class="text-2xl font-bold tracking-tight text-slate-700">
-                    Kursus Video
-                </h1>
-                <p class="pt-2 font-normal text-gray-500 dark:text-gray-400 ">Belajar tanpa batasan geografis dan waktu
-            </div>
-        </div>
-        {{-- card end 4 --}}
     </div>
 </div>
 {{-- card end --}}

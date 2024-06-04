a7710c9936f7106819fd6e69318cd190cf8671f1
saktiworkstation <saktiworkstation@gmail.com>
2024-06-04 20:53:06 +0700

    Added interest card

--- resources/views/landing-partials/interest.blade.php
+++ resources/views/landing-partials/interest.blade.php
@@ -1,1 +1,77 @@
 {{-- Interest pabe --}}
+{{-- card start --}}
+<div class="py-12 px-40 container bg-gradient-to-t from-slate-200">
+    <div class="row flex justify-center h-auto items-center ">
+        {{-- card start-1 --}}
+        <div class="max-w-sm p-4 items-center text-center">
+            <div class="p-2 bg-gradient-to-br h-28 from-blue-700 to-green-400 rounded-lg shadow content-center">
+                <h1 class="text-5xl font-bold tracking-tight text-white">
+                    31
+                </h1>
+            </div>
+            <div class="pt-5 max-w-sm">
+                <h1 class="text-2xl font-bold tracking-tight text-slate-700">
+                    Instruktur Terampil
+                </h1>
+                <p class="pt-2 font-normal text-gray-500 dark:text-gray-400 ">Mulai berlajar dari instruktur
+                    berpengalaman
+                </p>
+            </div>
+        </div>
+        {{-- card end-1 --}}
+
+        {{-- card start 2 --}}
+        <div class="max-w-sm p-4 items-center text-center">
+            <div class="p-2 bg-gradient-to-br h-28 from-rose-500 to-purple-500 rounded-lg shadow content-center">
+                <h1 class="text-5xl font-bold tracking-tight text-white">
+                    132
+                </h1>
+            </div>
+            <div class="pt-5 max-w-sm">
+                <h1 class="text-2xl font-bold tracking-tight text-slate-700">
+                    Siswa Bahagia
+                </h1>
+                <p class="pt-2 font-normal text-gray-500 dark:text-gray-400 ">Mendafar dalam kursus kami dan
+                    meningkatkan
+                    keterampilan mereka
+                </p>
+            </div>
+        </div>
+        {{-- card end 2 --}}
+
+        {{-- card start 3 --}}
+        <div class="max-w-sm p-4 items-center text-center">
+            <div class="p-2 bg-gradient-to-br h-28 from-emerald-500 to-green-500 rounded-lg shadow content-center">
+                <h1 class="text-5xl font-bold tracking-tight text-white">
+                    12
+                </h1>
+            </div>
+            <div class="pt-5 max-w-sm">
+                <h1 class="text-2xl font-bold tracking-tight text-slate-700">
+                    Kelas langsung
+                </h1>
+                <p class="pt-2 font-normal text-gray-500 dark:text-gray-400 ">Tingkatkan keterampilan anda dengan aliran
+                    pengetahuan langsung
+                </p>
+            </div>
+        </div>
+        {{-- card end 3 --}}
+
+        {{-- card start 4 --}}
+        <div class="max-w-sm p-4 items-center text-center">
+            <div class="p-2 bg-gradient-to-br h-28 from-rose-600 to-orange-300 rounded-lg shadow content-center">
+                <h1 class="text-5xl font-bold tracking-tight text-white">
+                    31
+                </h1>
+            </div>
+            <div class="pt-5 max-w-sm">
+                <h1 class="text-2xl font-bold tracking-tight text-slate-700">
+                    Kursus Video
+                </h1>
+                <p class="pt-2 font-normal text-gray-500 dark:text-gray-400 ">Belajar tanpa batasan geografis dan waktu
+            </div>
+        </div>
+        {{-- card end 4 --}}
+    </div>
+</div>
+{{-- card end --}}

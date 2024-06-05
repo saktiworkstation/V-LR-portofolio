cfb80c025743f64114d3d3072757eee6cf10a17c
saktiworkstation <saktiworkstation@gmail.com>
2024-06-05 16:21:20 +0700

    Start deploy data

--- resources/views/landing-partials/interest.blade.php
+++ resources/views/landing-partials/interest.blade.php
@@ -2,22 +2,16 @@
 {{-- card start --}}
 <div class="py-12 px-40 container bg-gradient-to-t from-slate-200">
     <div class="row flex justify-center h-auto items-center ">
-        {{-- card start-1 --}}
-        <div class="max-w-sm p-4 items-center text-center">
-            <div class="p-2 bg-gradient-to-br h-28 from-blue-700 to-green-400 rounded-lg shadow content-center">
-                <h1 class="text-5xl font-bold tracking-tight text-white">
-                    31
-                </h1>
+        @foreach ($interests as $item)
+            {{-- card start-1 --}}
+            <div class="max-w-sm p-4 items-center text-center">
+                <div class="pt-5 max-w-sm">
+                    <h1 class="text-2xl font-bold tracking-tight text-slate-700">
+                        {{ $item->interest }}
+                    </h1>
+                </div>
             </div>
-            <div class="pt-5 max-w-sm">
-                <h1 class="text-2xl font-bold tracking-tight text-slate-700">
-                    Instruktur Terampil
-                </h1>
-                <p class="pt-2 font-normal text-gray-500 dark:text-gray-400 ">Mulai berlajar dari instruktur
-                    berpengalaman
-                </p>
-            </div>
-        </div>
+        @endforeach
         {{-- card end-1 --}}
     </div>
 </div>

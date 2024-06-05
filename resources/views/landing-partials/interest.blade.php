687790b13d659070e1ef4f9b405e16027a4b1265
saktiworkstation <saktiworkstation@gmail.com>
2024-06-05 16:23:58 +0700

    Adding headline

--- resources/views/landing-partials/interest.blade.php
+++ resources/views/landing-partials/interest.blade.php
@@ -1,6 +1,7 @@
 {{-- Interest pabe --}}
 {{-- card start --}}
-<div class="py-12 px-40 container bg-gradient-to-t from-slate-200">
+<div class="py-12 px-40 container bg-gradient-to-t from-slate-200 text-center">
+    <h4 class="text-3xl md:text-5xl dark:text-black font-bold mb-6">Interest</h4>
     <div class="row flex justify-center h-auto items-center ">
         @foreach ($interests as $item)
             {{-- card start-1 --}}

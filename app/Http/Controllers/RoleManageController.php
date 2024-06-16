8a833add659c5a7a16f637590f2a7823caf3b03a
saktiworkstation <saktiworkstation@gmail.com>
2024-06-16 17:13:12 +0700

    send user data and roles

--- app/Http/Controllers/RoleManageController.php
+++ app/Http/Controllers/RoleManageController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RoleManageController extends Controller
 {
-    //
+    public function index(){
+        return view('role-manage.index', [
+            'datas' => User::with('roles')->get(),
+        ]);
+    }
 }

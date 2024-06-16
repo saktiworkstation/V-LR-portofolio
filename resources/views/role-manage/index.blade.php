ce51c047339d3546705b0f5496f31eff1c0ef77b
saktiworkstation <saktiworkstation@gmail.com>
2024-06-16 17:00:12 +0700

    adjustments in displaying data

--- resources/views/role-manage/index.blade.php
+++ resources/views/role-manage/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Interest') }}
+            {{ __('Role Manage') }}
         </h2>
     </x-slot>
 
@@ -9,31 +9,33 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    <x-link-primary-button :url="route('interest.create')">{{ __('Add New Interest') }}</x-link-primary-button>
+                    <x-link-primary-button :url="route('role-manage.create')">{{ __('Assign Role to USer') }}</x-link-primary-button>
                 </div>
             </div>
 
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-4xl">
                     <h4 class="pb-3 font-semibold text-xl text-gray-800 leading-tight">
-                        {{ __('Interest Data') }}
+                        {{ __('role-manage Data') }}
                     </h4>
                     <table class="border-collapse border-slate-500">
                         <thead class="bg-gray-800 text-white">
                             <tr>
-                                <th class="px-6 py-2 border border-slate-600">Interest</th>
+                                <th class="px-6 py-2 border border-slate-600">User</th>
+                                <th class="px-6 py-2 border border-slate-600">User Role</th>
                                 <th class="px-6 py-2 border border-slate-600">Action</th>
                             </tr>
                         </thead>
                         <tbody class="bg-gray-500">
                             @foreach ($datas as $data)
                                 <tr class="text-white">
-                                    <td class="px-6 py-2 border border-slate-700">{{ $data->interest }}</td>
+                                    <td class="px-6 py-2 border border-slate-700">Role data</td>
+                                    <td class="px-6 py-2 border border-slate-700">User data</td>
                                     <td class="px-6 py-2 border border-slate-700">
-                                        <a href="/interest/{{ $data->id }}/edit" class="">
+                                        <a href="/role-manage/{{ $data->id }}/edit" class="">
                                             Edit</span>
                                         </a>
-                                        <form action="/interest/{{ $data->id }}/delete" method="post"
+                                        <form action="/role-manage/{{ $data->id }}/delete" method="post"
                                             class="d-inline">
                                             @method('delete')
                                             @csrf

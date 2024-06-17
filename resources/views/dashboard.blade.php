6c7b21a01106698893982c328fbfcf7759c789f7
saktiworkstation <saktiworkstation@gmail.com>
2024-06-17 19:58:36 +0700

    Finished displaying user data

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,29 +24,32 @@
                         class="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
                         <div class="flex items-center justify-between mb-4">
                             <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest
-                                CUser CV
+                                User CV
                             </h5>
                         </div>
                         <div class="flow-root">
                             <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
                                 @foreach ($users as $user)
-                                    <li class="py-3 sm:py-4">
-                                        <div class="flex items-center">
-                                            <div class="flex-1 min-w-0 ms-4">
-                                                <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                                    Neil Sims
-                                                </p>
-                                                <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                                    email@windster.com
-                                                </p>
+                                    @if ($user->roles->contains('name', 'user'))
+                                        <li class="py-3 sm:py-4">
+                                            <div class="flex items-center">
+                                                <div class="flex-1 min-w-0 ms-4">
+                                                    <p
+                                                        class="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                                        {{ $user->name }}
+                                                    </p>
+                                                    <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                                                        {{ $user->email }}
+                                                    </p>
+                                                </div>
+                                                <div
+                                                    class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                                    <x-link-small-button
+                                                        :url="route('showcv')">{{ __('CV') }}</x-link-small-button>
+                                                </div>
                                             </div>
-                                            <div
-                                                class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                                <x-link-small-button
-                                                    :url="route('showcv')">{{ __('CV') }}</x-link-small-button>
-                                            </div>
-                                        </div>
-                                    </li>
+                                        </li>
+                                    @endif
                                 @endforeach
                             </ul>
                         </div>

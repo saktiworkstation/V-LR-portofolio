2b677dd12fef517d30e530802015a6ba6ce91902
saktiworkstation <saktiworkstation@gmail.com>
2024-06-17 18:21:36 +0700

    Customize the card

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,9 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-2 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    <div class="p-6 text-gray-900">
+                    <div class="p-2 text-gray-900 font-medium">
                         {{ __("You're logged in!") }}
                     </div>
                 </div>
@@ -18,18 +18,14 @@
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-4xl">
                     <h4 class="pb-3 font-semibold text-xl text-gray-800 leading-tight">
-                        {{ __('role-manage Data') }}
+                        {{ __('User CV List') }}
                     </h4>
                     <div
                         class="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
                         <div class="flex items-center justify-between mb-4">
                             <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Latest
-                                Customers
+                                CUser CV
                             </h5>
-                            <a href="#"
-                                class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
-                                View all
-                            </a>
                         </div>
                         <div class="flow-root">
                             <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
@@ -45,39 +41,8 @@
                                         </div>
                                         <div
                                             class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                            $320
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="py-3 sm:py-4">
-                                    <div class="flex items-center ">
-                                        <div class="flex-1 min-w-0 ms-4">
-                                            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                                Bonnie Green
-                                            </p>
-                                            <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                                email@windster.com
-                                            </p>
-                                        </div>
-                                        <div
-                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                            $3467
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="py-3 sm:py-4">
-                                    <div class="flex items-center">
-                                        <div class="flex-1 min-w-0 ms-4">
-                                            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                                Michael Gough
-                                            </p>
-                                            <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                                email@windster.com
-                                            </p>
-                                        </div>
-                                        <div
-                                            class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                            $67
+                                            <x-link-small-button
+                                                :url="route('showcv')">{{ __('CV') }}</x-link-small-button>
                                         </div>
                                     </div>
                                 </li>

f2538aad41c1d12bb937a46d229e98cb4e3c3132
saktiworkstation <saktiworkstation@gmail.com>
2024-06-17 18:21:15 +0700

    Added small button component

---
+++ resources/views/components/link-small-button.blade.php
@@ -0,0 +1,6 @@
+@props(['url', 'target' => '_self'])
+
+<a href="{{ $url }}" target="{{ $target }}"
+    {{ $attributes->merge(['class' => 'inline-flex items-center px-3 py-1 bg-gray-800 border border-transparent rounded-md font-medium text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+    {{ $slot }}
+</a>

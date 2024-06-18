5a8ed89e5bb50f9a418ec87fd9b7c09dd6c2646c
saktiworkstation <saktiworkstation@gmail.com>
2024-06-18 23:52:13 +0700

    Tet skill Data

--- resources/views/pdf/cv.blade.php
+++ resources/views/pdf/cv.blade.php
@@ -19,15 +19,11 @@
 
     <h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Data Skills</h2>
     <ul class="max-w-md space-y-1 text-gray-500 list-disc list-inside dark:text-gray-400">
-        <li>
-            At least 10 characters (and up to 100 characters)
-        </li>
-        <li>
-            At least one lowercase character
-        </li>
-        <li>
-            Inclusion of at least one special character, e.g., ! @ # ?
-        </li>
+        @foreach ($skills as $skill)
+            <li>
+                {{ $skill->name }}
+            </li>
+        @endforeach
     </ul>
 
     {{-- Flowbite --}}

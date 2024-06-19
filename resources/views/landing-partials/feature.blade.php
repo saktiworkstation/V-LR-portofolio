68723e3ceef3ebe7948c8363419ce3592081de39
Hyadawild <109930135+Hyadawild@users.noreply.github.com>
2024-06-19 08:28:04 +0700

    Update feature tehe

---
+++ resources/views/landing-partials/feature.blade.php
@@ -0,0 +1,195 @@
+// tehe
+
+<!-- ====== Portfolio Section Start -->
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+    AOS.init();
+</script>
+
+<section class="pt-20 pb-12 lg:pt-[120px] lg:pb-[30px] bg-white dark:bg-dark" id="feature">
+    <div class="container mx-auto max-w-[100%]">
+        <div class="-mx-4 flex flex-wrap">
+            <div class="w-full px-4">
+                <div class="mx-auto mb-[40px] max-w-[510px] text-center">
+                    <h2 class="text-dark mb-3 text-3xl leading-[1.208] font-bold sm:text-4xl md:text-[40px] animate-bounce">
+                        Our Feature
+                    </h2>
+                    <p class="text-body-color text-base dark:text-dark-6">
+                        The following are the main features that we offer to you.
+                        We are here to make it easier for you to create a curriculum vitae
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="flex justify-center flex-wrap mx-auto w-[80%]">
+            <div
+                class="block w-3/12 mx-4 rounded-lg bg-white text-center text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white">
+                <div class="px-4 pt-4 flex flex-wrap">
+                    <img src="/img/instant-1.png" alt="" class="w-[175px]  mx-auto">
+                </div>
+                <div class="p-6">
+                    <h5 class="mb-2 text-xl text-black font-medium leading-tight ">
+                    Non-stop Service
+                    </h5>
+                    <p class="mb-4 text-base text-black ">
+                    Our service operates nonstop, 24 hours a day, 7 days a week, which you can access at any time.
+                    </p>
+                </div>
+            </div>
+            <div
+                class="block w-3/12 mx-4 rounded-lg bg-white text-center text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white">
+                <div class="px-4 pt-4 flex flex-wrap">
+                    <img src="/img/easy-1.png" alt=""  class="w-[175px]  mx-auto">
+                </div>
+                <div class="p-6">
+                    <h5 class="mb-2 text-xl text-black font-medium leading-tight ">
+                    Instan Creation
+                    </h5>
+                    <p class="mb-4 text-base text-black ">
+                    Our instant services are designed to meet your needs quickly and efficiently
+                    </p>
+                </div>
+            </div>
+            <div
+                class="block w-3/12 mx-4 rounded-lg bg-white text-center text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white">
+                <div class="px-4 pt-4 flex flex-wrap">
+                    <img src="/img/free-1.png" alt=""  class="w-[175px]  mx-auto">
+                </div>
+                <div class="p-6">
+                    <h5 class="mb-2 text-xl text-black font-medium leading-tight ">
+                    Free For All
+                    </h5>
+                    <p class="mb-4 text-base text-black ">
+                    Our service proudly offers free access to everyone, ensuring no one is excluded from its benefits.
+                    </p>
+                </div>
+            </div>
+        </div>
+        
+    <!-- Section: Design Block -->
+    <section class="mb-32 w-[100%] mt-20 mx-auto md:px-6 ">
+        <div class="p-8 pb-4 flex flex-wrap ">
+        <div class=" flex-none lg:w-1/12"></div>
+        <div class="mb-6 shrink-0 grow-0 basis-auto lg:mb-0 lg:w-3/12 lg:pr-6">
+            <div
+            class="ripple relative overflow-hidden rounded-lg bg-cover bg-[50%] bg-no-repeat"
+            data-te-ripple-init data-te-ripple-color="light">
+            <img src="/img/instant-2.png" class="w-full" alt="Louvre" />
+            <a href="#!">
+                <div
+                class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
+                </div>
+            </a>
+            </div>
+        </div>
+
+        <div class="shrink-0 grow-0 basis-auto lg:w-7/12 pt-8 pl-10 text-left lg:pr-10">
+            <h3 class="mb-4 text-2xl font-bold">
+            <a href="#!">Non-stop Services</a>
+            </h3>
+            <p class="mb-6 text-neutral-500 dark:text-neutral-900 text-md">
+            Our services operate non-stop, 24 hours a day, 7 days a week, ensuring that you have access to our support and resources whenever you need them. 
+            Whether day or night, weekdays or weekends, our dedicated team is always ready to help you 24-hour service ensures that you can rely on us at any time.
+            </p>
+            <button
+            type="button"
+            class="inline-block rounded bg-blue-700 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-dark-3 transition duration-150 ease-in-out hover:bg-blue-900 hover:shadow-dark-2 focus:bg-blue-900 focus:shadow-dark-2 focus:outline-none focus:ring-0">
+            View More
+            </button>
+        </div>
+        </div>
+
+        <div class=" p-8 pb-4  flex flex-wrap lg:flex-row-reverse">
+        <div class=" flex-none lg:w-1/12"></div>
+        <div class="mb-6 shrink-0 grow-0 basis-auto lg:mb-0 lg:w-3/12 lg:pl-6">
+            <div
+            class="ripple relative overflow-hidden rounded-lg bg-cover bg-[50%] bg-no-repeat"
+            data-te-ripple-init data-te-ripple-color="light">
+            <img src="/img/easy-2.png" class="w-full" alt="Louvre" />
+            <a href="#!">
+                <div
+                class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
+                </div>
+            </a>
+            </div>
+        </div>
+
+        <div class="shrink-0 grow-0 basis-auto lg:w-7/12 pt-8 pl-10 text-left lg:pr-10">
+            <h3 class="mb-4 text-2xl font-bold">
+            <a href="#!">Instan Creation</a>
+            </h3>
+            <p class=" mb-6 text-neutral-500 dark:text-neutral-900 text-md">
+            Our instant services are designed to meet your needs quickly and efficiently.
+            With just a few clicks, you can access a variety of features that simplify tasks and save you valuable time. 
+            Experience the convenience of real-time functions and see how our instant services can increase your productivity. 
+            </p>
+            <button
+            type="button"
+            class="inline-block rounded bg-blue-700 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-dark-3 transition duration-150 ease-in-out hover:bg-blue-900 hover:shadow-dark-2 focus:bg-blue-900 focus:shadow-dark-2 focus:outline-none focus:ring-0">
+            View More
+            </button>
+        </div>
+
+        </div>
+
+        <div class="p-8 pb-4  flex flex-wrap">
+            <div class=" flex-none lg:w-1/12"> </div>
+            <div class="mb-6 shrink-0 grow-0 basis-auto lg:mb-0 lg:w-3/12 lg:pr-6">
+            <div
+            class="ripple relative overflow-hidden rounded-lg bg-cover bg-[50%] bg-no-repeat "
+            data-te-ripple-init data-te-ripple-color="light">
+            <img src="/img/free-2.png" class="w-full" alt="Louvre" />
+            <a href="#!">
+                <div
+                class="absolute top-0 right-0 bottom-0 left-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
+                </div>
+            </a>
+            </div>
+        </div>
+
+        <div class="shrink-0 grow-0 basis-auto lg:w-7/12 pt-8 pl-10 text-left lg:pr-10">
+            <h3 class="mb-4 text-2xl font-bold">
+            <a href="#!">Free For All</a>
+            </h3>
+            <p class="mb-6 text-neutral-500 dark:text-neutral-900 text-md">
+            Our service proudly offers free access to everyone, ensuring no one is excluded from its benefits. 
+            Whether you are a student, professional, or just someone looking to explore new tools, 
+            you can enjoy all our features at no cost to you. 
+            </p>
+            <button
+            type="button"
+            class="inline-block rounded bg-blue-700 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-50 shadow-dark-3 transition duration-150 ease-in-out hover:bg-blue-900 hover:shadow-dark-2 focus:bg-blue-900 focus:shadow-dark-2 focus:outline-none focus:ring-0">
+            View More
+            </button>
+            </div>
+            </div>
+        </section>
+    <!-- Section: Design Block -->
+</div>
+    </div>
+</section>
+<!-- ====== Portfolio Section End -->
+
+<style>
+    @keyframes bounce {
+
+        0%,
+        100% {
+            transform: translateY(0);
+        }
+
+        50% {
+            transform: translateY(-10px);
+            /* Bounce height */
+        }
+    }
+
+    .animate-bounce {
+        animation: bounce 1.2s ease-in-out infinite;
+        /* Animation settings */
+    }
+</style>
+
+
+<!-- ====== Portfolio style Section End -->

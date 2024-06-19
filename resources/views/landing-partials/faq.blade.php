d5e3e6ed7eb399d625d063a1eb812adf980f3aae
YSindhunata <ysindhunata@gmail.com>
2024-06-19 18:43:12 +0700

    add faq section

---
+++ resources/views/landing-partials/faq.blade.php
@@ -0,0 +1,189 @@
+<!-- faq -->
+
+<div class="bg-white pb-60 pt-20 px-40 " id="contact">
+    <div class="-mx-4 flex flex-wrap">
+        <div class="w-full px-4 mb-10">
+            <div class="mx-auto mb-[40px] max-w-[510px] text-center">
+                <h2 class="text-dark mb-3 text-3xl leading-[1.208] font-bold sm:text-4xl md:text-[40px] animate-bounce">
+                Frequently Asked Questions
+                </h2>
+                <p class="text-body-color text-base dark:text-dark-6 ">
+                    There are many variations of passages of Lorem Ipsum available
+                    but the majority have suffered alteration in some form.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="max-w-[75%] mx-auto bg-white flex flex-wrap">
+        <div class="w-1/2">
+        <div class="max-w-lg max-h-[250px] p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <h3 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Let's Talk</h3>
+            </a>
+            <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Have some big idea or brand to develop and need help? Then reach out we'd love to hear about your project  and provide help.</p>
+            <a href="#" class="inline-flex font-medium items-center text-blue-600 hover:underline">
+                See our guideline
+                <svg class="w-3 h-3 ms-2.5 rtl:rotate-[270deg]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"/>
+                </svg>
+            </a>
+        </div> <div class="w-full px-4 mt-10">
+            <div class="mt-12">
+                <h1 class="text-gray-800 text-2xl font-bold">Socials</h1>
+                    <ul class="flex mt-4 space-x-4">
+                        <li class="bg-[#e6e6e6cf] h-11 w-11 rounded-full flex items-center justify-center shrink-0">
+                            <a href="javascript:void(0)">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill='#007bff'
+                                    viewBox="0 0 24 24">
+                                    <path
+                                        d="M6.812 13.937H9.33v9.312c0 .414.335.75.75.75l4.007.001a.75.75 0 0 0 .75-.75v-9.312h2.387a.75.75 0 0 0 .744-.657l.498-4a.75.75 0 0 0-.744-.843h-2.885c.113-2.471-.435-3.202 1.172-3.202 1.088-.13 2.804.421 2.804-.75V.909a.75.75 0 0 0-.648-.743A26.926 26.926 0 0 0 15.071 0c-7.01 0-5.567 7.772-5.74 8.437H6.812a.75.75 0 0 0-.75.75v4c0 .414.336.75.75.75zm.75-3.999h2.518a.75.75 0 0 0 .75-.75V6.037c0-2.883 1.545-4.536 4.24-4.536.878 0 1.686.043 2.242.087v2.149c-.402.205-3.976-.884-3.976 2.697v2.755c0 .414.336.75.75.75h2.786l-.312 2.5h-2.474a.75.75 0 0 0-.75.75V22.5h-2.505v-9.312a.75.75 0 0 0-.75-.75H7.562z"
+                                        data-original="#000000" />
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="bg-[#e6e6e6cf] h-11 w-11 rounded-full flex items-center justify-center shrink-0">
+                            <a href="javascript:void(0)">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill='#007bff'
+                                    viewBox="0 0 511 512">
+                                    <path
+                                        d="M111.898 160.664H15.5c-8.285 0-15 6.719-15 15V497c0 8.285 6.715 15 15 15h96.398c8.286 0 15-6.715 15-15V175.664c0-8.281-6.714-15-15-15zM96.898 482H30.5V190.664h66.398zM63.703 0C28.852 0 .5 28.352.5 63.195c0 34.852 28.352 63.2 63.203 63.2 34.848 0 63.195-28.352 63.195-63.2C126.898 28.352 98.551 0 63.703 0zm0 96.395c-18.308 0-33.203-14.891-33.203-33.2C30.5 44.891 45.395 30 63.703 30c18.305 0 33.195 14.89 33.195 33.195 0 18.309-14.89 33.2-33.195 33.2zm289.207 62.148c-22.8 0-45.273 5.496-65.398 15.777-.684-7.652-7.11-13.656-14.942-13.656h-96.406c-8.281 0-15 6.719-15 15V497c0 8.285 6.719 15 15 15h96.406c8.285 0 15-6.715 15-15V320.266c0-22.735 18.5-41.23 41.235-41.23 22.734 0 41.226 18.495 41.226 41.23V497c0 8.285 6.719 15 15 15h96.403c8.285 0 15-6.715 15-15V302.066c0-79.14-64.383-143.523-143.524-143.523zM466.434 482h-66.399V320.266c0-39.278-31.953-71.23-71.226-71.23-39.282 0-71.239 31.952-71.239 71.23V482h-66.402V190.664h66.402v11.082c0 5.77 3.309 11.027 8.512 13.524a15.01 15.01 0 0 0 15.875-1.82c20.313-16.294 44.852-24.907 70.953-24.907 62.598 0 113.524 50.926 113.524 113.523zm0 0"
+                                        data-original="#000000" />
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="bg-[#e6e6e6cf] h-11 w-11 rounded-full flex items-center justify-center shrink-0">
+                            <a href="javascript:void(0)">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill='#007bff'
+                                    viewBox="0 0 24 24">
+                                    <path
+                                        d="M12 9.3a2.7 2.7 0 1 0 0 5.4 2.7 2.7 0 0 0 0-5.4Zm0-1.8a4.5 4.5 0 1 1 0 9 4.5 4.5 0 0 1 0-9Zm5.85-.225a1.125 1.125 0 1 1-2.25 0 1.125 1.125 0 0 1 2.25 0ZM12 4.8c-2.227 0-2.59.006-3.626.052-.706.034-1.18.128-1.618.299a2.59 2.59 0 0 0-.972.633 2.601 2.601 0 0 0-.634.972c-.17.44-.265.913-.298 1.618C4.805 9.367 4.8 9.714 4.8 12c0 2.227.006 2.59.052 3.626.034.705.128 1.18.298 1.617.153.392.333.674.632.972.303.303.585.484.972.633.445.172.918.267 1.62.3.993.047 1.34.052 3.626.052 2.227 0 2.59-.006 3.626-.052.704-.034 1.178-.128 1.617-.298.39-.152.674-.333.972-.632.304-.303.485-.585.634-.972.171-.444.266-.918.299-1.62.047-.993.052-1.34.052-3.626 0-2.227-.006-2.59-.052-3.626-.034-.704-.128-1.18-.299-1.618a2.619 2.619 0 0 0-.633-.972 2.595 2.595 0 0 0-.972-.634c-.44-.17-.914-.265-1.618-.298-.993-.047-1.34-.052-3.626-.052ZM12 3c2.445 0 2.75.009 3.71.054.958.045 1.61.195 2.185.419A4.388 4.388 0 0 1 19.49 4.51c.457.45.812.994 1.038 1.595.222.573.373 1.227.418 2.185.042.96.054 1.265.054 3.71 0 2.445-.009 2.75-.054 3.71-.045.958-.196 1.61-.419 2.185a4.395 4.395 0 0 1-1.037 1.595 4.44 4.44 0 0 1-1.595 1.038c-.573.222-1.227.373-2.185.418-.96.042-1.265.054-3.71.054-2.445 0-2.75-.009-3.71-.054-.958-.045-1.61-.196-2.185-.419A4.402 4.402 0 0 1 4.51 19.49a4.414 4.414 0 0 1-1.037-1.595c-.224-.573-.374-1.227-.419-2.185C3.012 14.75 3 14.445 3 12c0-2.445.009-2.75.054-3.71s.195-1.61.419-2.185A4.392 4.392 0 0 1 4.51 4.51c.45-.458.994-.812 1.595-1.037.574-.224 1.226-.374 2.185-.419C9.25 3.012 9.555 3 12 3Z">
+                                    </path>
+                                </svg>
+                            </a>
+                        </li>
+                        <li class="bg-[#e6e6e6cf] h-11 w-11 rounded-full flex items-center justify-center shrink-0">
+                            <a href="javascript:void(0)">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"  fill='#007bff' id="github">
+                                <path fill-rule="evenodd" 
+                                    d="M10 0c5.523 0 10 4.59 10 10.253 0 4.529-2.862 8.371-6.833 9.728-.507.101-.687-.219-.687-.492 0-.338.012-1.442.012-2.814 0-.956-.32-1.58-.679-1.898 2.227-.254 4.567-1.121 4.567-5.059 0-1.12-.388-2.034-1.03-2.752.104-.259.447-1.302-.098-2.714 0 0-.838-.275-2.747 1.051A9.396 9.396 0 0 0 10 4.958a9.375 9.375 0 0 0-2.503.345C5.586 3.977 4.746 4.252 4.746 4.252c-.543 1.412-.2 2.455-.097 2.714-.639.718-1.03 1.632-1.03 2.752 0 3.928 2.335 4.808 4.556 5.067-.286.256-.545.708-.635 1.371-.57.262-2.018.715-2.91-.852 0 0-.529-.985-1.533-1.057 0 0-.975-.013-.068.623 0 0 .655.315 1.11 1.5 0 0 .587 1.83 3.369 1.21.005.857.014 1.665.014 1.909 0 .271-.184.588-.683.493C2.865 18.627 0 14.783 0 10.253 0 4.59 4.478 0 10 0">
+                                </path>
+                            </svg>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+
+        </div>
+        <div class="w-1/2">  
+            
+            <div id="accordion-flush" data-accordion="collapse" data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
+            <h2 id="accordion-flush-heading-1">
+                <button type="button" class="flex items-center justify-between w-full py-5 pr-2 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3" data-accordion-target="#accordion-flush-body-1" aria-expanded="true" aria-controls="accordion-flush-body-1">
+                <span>How to make a CV?</span>
+                <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+                </svg>
+                </button>
+            </h2>
+            <div id="accordion-flush-body-1" class="hidden px-2" aria-labelledby="accordion-flush-heading-1">
+                <div class="py-5 border-b border-gray-200 dark:border-gray-700">
+                    <ul>
+                        <li class="mb-2 text-gray-500 dark:text-gray-400">
+                            <span class="font-semibold">Create an Account:</span>
+                            Visit the website or application where you need to create an account. Look for the "Sign Up" or "Register" button and click on it. Fill in the required information, such as your email address, username, and password. Follow the instructions to verify your email if needed.
+                        </li>
+                        <li class="mb-2 text-gray-500 dark:text-gray-400">
+                            <span class="font-semibold">Fill in Personal Information:</span>
+                            Once your account is created, log in and navigate to your profile or account settings. Fill in your personal information, including your name, contact details, education, work experience, skills, and any other relevant information.
+                        </li>
+                        <li class="mb-2 text-gray-500 dark:text-gray-400">
+                            <span class="font-semibold">Print CV:</span> 
+                            After entering all your information, look for an option to generate or export your CV. This might be labeled as "Generate CV," "Download CV," or similar. Click on this option, review the generated document to ensure all information is correct, and then print it.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <h2 id="accordion-flush-heading-2">
+                <button type="button" class="flex items-center justify-between w-full py-5 pr-2 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3" data-accordion-target="#accordion-flush-body-2" aria-expanded="false" aria-controls="accordion-flush-body-2">
+                <span>Is this possible for lay people?</span>
+                <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+                </svg>
+                </button>
+            </h2>
+            <div id="accordion-flush-body-2" class="hidden px-2" aria-labelledby="accordion-flush-heading-2">
+                <div class="py-5 border-b border-gray-200 dark:border-gray-700">
+                <p class="mb-2 text-gray-500 dark:text-gray-400">
+                    Yes, this can be used for individuals without specialized knowledge can successfully understand or accomplish a particular task or concept. 
+                    Essentially, it's asking if something is accessible or achievable for the average person without needing extensive training or expertise.</p>
+                </div>
+            </div>
+            <h2 id="accordion-flush-heading-3">
+                <button type="button" class="flex items-center justify-between w-full py-5 pr-2 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3" data-accordion-target="#accordion-flush-body-3" aria-expanded="false" aria-controls="accordion-flush-body-3">
+                <span>What makes this website different from other websites?</span>
+                <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+                </svg>
+                </button>
+            </h2>
+            <div id="accordion-flush-body-3" class="hidden px-2" aria-labelledby="accordion-flush-heading-3">
+                <div class="py-5 border-b border-gray-200 dark:border-gray-700">
+                <p class="mb-2 text-gray-500 dark:text-gray-400">
+                    This website provides various services such as 24 hour non-stop services, easy-to-use tools, and instant creation.
+                </p>
+                </div>
+            </div>
+            <h2 id="accordion-flush-heading-4">
+                <button type="button" class="flex items-center justify-between w-full py-5 pr-2 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3" data-accordion-target="#accordion-flush-body-4" aria-expanded="false" aria-controls="accordion-flush-body-4">
+                <span>Is my data safe on your website?</span>
+                <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+                </svg>
+                </button>
+            </h2>
+            <div id="accordion-flush-body-4" class="hidden px-2" aria-labelledby="accordion-flush-heading-4">
+                <div class="py-5 border-b border-gray-200 dark:border-gray-700">
+                    <p class="mb-2 text-gray-500 dark:text-gray-400">Yes, your data is safe with us. We use advanced encryption and security measures to protect your personal information. Your data will not be shared with third parties without your consent.</p>
+                </div>
+            </div>
+            <h2 id="accordion-flush-heading-5">
+                <button type="button" class="flex items-center justify-between w-full py-5 pr-2 font-medium rtl:text-right text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400 gap-3" data-accordion-target="#accordion-flush-body-5" aria-expanded="false" aria-controls="accordion-flush-body-3">
+                <span>How can I contact customer support if I have an issue?</span>
+                <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+                </svg>
+                </button>
+            </h2>
+            <div id="accordion-flush-body-5" class="hidden px-2" aria-labelledby="accordion-flush-heading-3">
+                <div class="py-5 border-b border-gray-200 dark:border-gray-700">
+                    <p class="mb-2 text-gray-500 dark:text-gray-400">If you have any issues or questions, you can contact our customer support team via the 'Contact Us' section on our website. Alternatively, you can email us at <span class="text-blue-700 underline">vlr.corp@example.com</span> Our support team is available 24/7 and will respond to your query as soon as possible.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- ====== rating Section End -->
+
+<style>
+    @keyframes bounce {
+
+        0%,
+        100% {
+            transform: translateY(0);
+        }
+
+        50% {
+            transform: translateY(-10px);
+            /* Bounce height */
+        }
+    }
+
+    .animate-bounce {
+        animation: bounce 1.2s ease-in-out infinite;
+        /* Animation settings */
+    }
+</style>

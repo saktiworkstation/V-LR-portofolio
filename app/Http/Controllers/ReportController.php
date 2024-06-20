d6c75ee748720f4e6306386731578d0f5766f59d
saktiworkstation <saktiworkstation@gmail.com>
2024-06-20 16:47:05 +0700

    added comand to access index report page

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Skill;
+use App\Models\Project;
+use App\Models\Interest;
+use App\Models\Education;
+use App\Models\Experience;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
     public function index(){
-        // 
+        return view('report.index', [
+            'skills' => Skill::latest()->get(),
+            'projects' => Project::latest()->get(),
+            'educations' => Education::latest()->get(),
+            'intereses' => Interest::latest()->get(),
+            'experineces' => Experience::latest()->get(),
+        ]);
     }
 }

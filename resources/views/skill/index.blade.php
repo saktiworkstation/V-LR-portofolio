f0ab9616eb75358bc5791f6c866edbd0c830a484
saktiworkstation <saktiworkstation@gmail.com>
2024-06-20 14:06:29 +0700

    zero data skill handler

--- resources/views/skill/index.blade.php
+++ resources/views/skill/index.blade.php
@@ -33,27 +33,33 @@
                             </tr>
                         </thead>
                         <tbody class="bg-gray-500">
-                            @foreach ($datas as $data)
-                                <tr class="text-white">
-                                    <td class="px-6 py-2 border border-slate-700">{{ $data->name }}</td>
-                                    <td class="px-6 py-2 border border-slate-700">{{ $data->description }}</td>
-                                    <td class="px-6 py-2 border border-slate-700">{{ $data->skill_level }}</td>
-                                    <td class="px-6 py-2 border border-slate-700">
-                                        <a href="/skill/{{ $data->id }}/edit" class="">
-                                            Edit</span>
-                                        </a>
-                                        <form action="/skill/{{ $data->id }}/delete" method="post"
-                                            class="d-inline">
-                                            @method('delete')
-                                            @csrf
-                                            <button class=""
-                                                onclick="return confirm('Are you sure want to delete {{ $data->name }}?')">
-                                                Hapus</span>
-                                            </button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
+                            @if ($datas->count() > 0)
+                                @foreach ($datas as $data)
+                                    <tr class="text-white">
+                                        <td class="px-6 py-2 border border-slate-700">{{ $data->name }}</td>
+                                        <td class="px-6 py-2 border border-slate-700">{{ $data->description }}</td>
+                                        <td class="px-6 py-2 border border-slate-700">{{ $data->skill_level }}</td>
+                                        <td class="px-6 py-2 border border-slate-700">
+                                            <a href="/skill/{{ $data->id }}/edit" class="">
+                                                Edit</span>
+                                            </a>
+                                            <form action="/skill/{{ $data->id }}/delete" method="post"
+                                                class="d-inline">
+                                                @method('delete')
+                                                @csrf
+                                                <button class=""
+                                                    onclick="return confirm('Are you sure want to delete {{ $data->name }}?')">
+                                                    Hapus</span>
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @else
+                                <h4 class="pb-3 font-semibold text-base text-red-500 leading-tight">
+                                    {{ __('You dont have a Skill yet.') }}
+                                </h4>
+                            @endif
                         </tbody>
                     </table>
                 </div>

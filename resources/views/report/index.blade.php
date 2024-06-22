a7e1aab9037a0218dd0e8a7010e3b76c68a819ce
saktiworkstation <saktiworkstation@gmail.com>
2024-06-22 15:04:50 +0700

    added button to get pdf report

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -19,7 +19,7 @@
                     @endif
                     {{-- content --}}
 
-                    <div class="max-w-3xl w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
+                    <div class="max-w-5xl w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
                         <div class="flex justify-between mb-3">
                             <div class="flex items-center">
                                 <div class="flex justify-center items-center">
@@ -31,7 +31,7 @@
                         </div>
 
                         <div class="bg-gray-50 dark:bg-gray-700 p-3 rounded-lg">
-                            <div class="grid grid-cols-5 gap-3 mb-2">
+                            <div class="grid grid-cols-5 gap-4 mb-2">
                                 <dl class="bg-lime-100 rounded-lg flex flex-col items-center justify-center h-[78px]">
                                     <dt
                                         class="w-8 h-8 rounded-full bg-lime-200 text-lime-600 text-sm font-medium flex items-center justify-center mb-1">
@@ -78,55 +78,21 @@
                             class="grid grid-cols-1 items-center border-gray-200 border-t dark:border-gray-700 justify-between">
                             <div class="flex justify-between items-center pt-5">
                                 <!-- Button -->
-                                <button id="dropdownDefaultButton" data-dropdown-toggle="lastDaysdropdown"
-                                    data-dropdown-placement="bottom"
-                                    class="text-sm font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 text-center inline-flex items-center dark:hover:text-white"
-                                    type="button">
-                                    Last 7 days
-                                    <svg class="w-2.5 m-2.5 ms-1.5" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="2" d="m1 1 4 4 4-4" />
-                                    </svg>
-                                </button>
-                                <div id="lastDaysdropdown"
-                                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200"
-                                        aria-labelledby="dropdownDefaultButton">
-                                        <li>
-                                            <a href="#"
-                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Yesterday</a>
-                                        </li>
-                                        <li>
-                                            <a href="#"
-                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Today</a>
-                                        </li>
-                                        <li>
-                                            <a href="#"
-                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last
-                                                7 days</a>
-                                        </li>
-                                        <li>
-                                            <a href="#"
-                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last
-                                                30 days</a>
-                                        </li>
-                                        <li>
-                                            <a href="#"
-                                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Last
-                                                90 days</a>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <a href="#"
-                                    class="uppercase text-sm font-semibold inline-flex items-center rounded-lg text-blue-600 hover:text-blue-700 dark:hover:text-blue-500  hover:bg-gray-100 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 px-3 py-2">
-                                    Progress report
-                                    <svg class="w-2.5 h-2.5 ms-1.5 rtl:rotate-180" aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                            stroke-width="2" d="m1 9 4-4-4-4" />
-                                    </svg>
-                                </a>
+                                <x-link-primary-button :url="route('report')">
+                                    {{ __('Skill Report') }}
+                                </x-link-primary-button>
+                                <x-link-primary-button :url="route('report')">
+                                    {{ __('Project Report') }}
+                                </x-link-primary-button>
+                                <x-link-primary-button :url="route('report')">
+                                    {{ __('Education Report') }}
+                                </x-link-primary-button>
+                                <x-link-primary-button :url="route('report')">
+                                    {{ __('Interest Report') }}
+                                </x-link-primary-button>
+                                <x-link-primary-button :url="route('report')">
+                                    {{ __('Experience Report') }}
+                                </x-link-primary-button>
                             </div>
                         </div>
                     </div>

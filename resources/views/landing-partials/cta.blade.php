d81f9833aa2a7900474ec70949777669687e6fcb
Hyadawild <kudosinichi9290@gmail.com>
2024-06-23 18:47:29 +0700

    add particle and fix card position

--- resources/views/landing-partials/cta.blade.php
+++ resources/views/landing-partials/cta.blade.php
@@ -3,7 +3,7 @@
 <div class="max-w-[50%] h-[300px] mx-auto p-6 pt-10 bg-white border border-gray-200  shadow-inner shadow-neutral-200 rounded-lg dark:bg-gray-800 dark:border-gray-700">
     <div class="mx-auto max-w-screen-xl ">
         <div class="max-w-screen-md text-center mx-auto">
-            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Let's Start Make Your CV With Us.</h2>
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white animate-bounce">Let's Start Make Your CV With Us.</h2>
             <p class="mb-8 mt-6 font-light text-gray-500 sm:text-xl dark:text-gray-400">We are here to help you create a CV effectively and efficiently. so you can continue your productivity.</p>
             <div class="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4 justify-center">
                 <a href="#" class="inline-flex items-center justify-center px-4 py-2.5 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
@@ -13,3 +13,23 @@
         </div>
     </div>
 </div>
+
+<style>
+    @keyframes bounce {
+
+        0%,
+        100% {
+            transform: translateY(0);
+        }
+
+        50% {
+            transform: translateY(-5px);
+            /* Bounce height */
+        }
+    }
+
+    .animate-bounce {
+        animation: bounce 1.2s ease-in-out infinite;
+        /* Animation settings */
+    }
+</style>

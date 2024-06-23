7626bdb752733c700efc6b7798b9e3f26cdb1f5d
saktiworkstation <saktiworkstation@gmail.com>
2024-06-23 13:45:14 +0700

    Create skill report page

---
+++ resources/views/report/skill.blade.php
@@ -0,0 +1,61 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Skill') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-4xl">
+                    <h4 class="pb-3 font-semibold text-xl text-gray-800 leading-tight">
+                        {{ __('Skill Data') }}
+                    </h4>
+                    @if (session()->has('success'))
+                        <x-success-alert>
+                            {{ session('success') }}
+                        </x-success-alert>
+                    @endif
+                    <table class="border-collapse border-slate-500">
+                        <thead class="bg-gray-800 text-white">
+                            <tr>
+                                <th class="px-6 py-2 border border-slate-600">User</th>
+                                <th class="px-6 py-2 border border-slate-600">skill Name</th>
+                                <th class="px-6 py-2 border border-slate-600">Desciption</th>
+                                <th class="px-6 py-2 border border-slate-600">Skill Level</th>
+                                <th class="px-6 py-2 border border-slate-600">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-gray-500">
+                            @if ($datas->count() > 0)
+                                @foreach ($datas as $data)
+                                    <tr class="text-white">
+                                        <td class="px-6 py-2 border border-slate-700">{{ $data->User->name }}</td>
+                                        <td class="px-6 py-2 border border-slate-700">{{ $data->name }}</td>
+                                        <td class="px-6 py-2 border border-slate-700">{{ $data->description }}</td>
+                                        <td class="px-6 py-2 border border-slate-700">{{ $data->skill_level }}</td>
+                                        <td class="px-6 py-2 border border-slate-700">
+                                            <form action="/skill/{{ $data->id }}/delete" method="post"
+                                                class="d-inline">
+                                                @method('delete')
+                                                @csrf
+                                                <x-delete-button :message="$data->name">
+                                                    Delete
+                                                </x-delete-button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @else
+                                <h4 class="pb-3 font-semibold text-base text-red-500 leading-tight">
+                                    {{ __('There no Skill yet.') }}
+                                </h4>
+                            @endif
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>

65b6b4d95ffa2b498874629affc34c75c19c984e
saktiworkstation <saktiworkstation@gmail.com>
2024-06-24 16:46:01 +0700

    Create personal data controller

---
+++ app/Http/Controllers/PersonalDataController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PersonalDataController extends Controller
+{
+    public function index(){
+        // form
+    }
+
+    public function update(){
+        // store update data
+    }
+}

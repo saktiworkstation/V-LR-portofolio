a953ab1d757e1204a1b6557218df624ecc516f93
saktiworkstation <saktiworkstation@gmail.com>
2024-06-24 20:31:08 +0700

    added command to access form, and added conditioning

--- app/Http/Controllers/PersonalDataController.php
+++ app/Http/Controllers/PersonalDataController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PersonalDataController extends Controller
 {
     public function index(){
-        // form
+        if(Auth::user()->roles->contains('name', 'user')){
+            return view('personal-data.index',[
+                'data' => Auth::user(),
+            ]);
+        }else{
+            return redirect('report/interest')->with('success', 'Cant Access');
+        }
     }
 
     public function update(){

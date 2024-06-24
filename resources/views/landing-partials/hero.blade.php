ad13432dc2905b61774371493f53e3fe1fca1a07
Hyadawild <kudosinichi9290@gmail.com>
2024-06-24 20:40:00 +0700

    backup hero responsive

--- resources/views/landing-partials/hero.blade.php
+++ resources/views/landing-partials/hero.blade.php
@@ -1,3 +1,32 @@
+{{--  backup hero start 
+<div class="mx-auto py-52 bg-slate-800 relative" data-aos="fade-down-left" id="hero">
+    <div id="particles-js" class="absolute top-0 left-0 w-full h-full z-0"></div>
+    <div class="row max-w-[90%] mx-auto flex justify-center h-auto items-center relative z-10">
+        <div class="w-1/2 ps-40 pe-5 py-5" data-aos="fade-up" data-aos-delay="200">
+         
+            <h1 class="text-8xl font-bold mb-5 text-slate-100 mt-8">
+                Create Your CV <span class="">Practically and Easily</span>
+            </h1>
+            <p class="text-slate-500 mb-4 text-justify pe-10">
+            Do you want to create a CV simply and easily for yourself? Look no further! 
+            We have the perfect tools and features to help you create a CV that truly represents you. 
+            Our user-friendly interface allows you to showcase your work and achievements in a professional and organized manner. 
+            So why wait? Register now and start building your CV in just a few clicks!
+            </p>
+
+            
+            <a href="#" rel="noopener noreferrer"
+            class="uppercase w-auto h-[45px] inline-flex items-center px-4 py-3 bg-slate-100 border border-transparent rounded-md font-semibold text-md text-grey-500 tracking-widest hover:bg-neutral-300 my-5">
+                create Now &rarr;
+            </a>
+        </div>
+        <div class="w-1/2" data-aos="fade-left" data-aos-delay="400">
+            <img src="/img/banner.png" alt="Be Good" class="w-6/6">
+        </div>
+    </div>
+</div>
+--}}
+
 <!-- particles.js library -->
 <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
 <!-- AOS CSS -->

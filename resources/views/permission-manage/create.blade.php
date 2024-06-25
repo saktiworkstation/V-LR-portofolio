89809d1f3979040b65717416c699ed7a0a07e50e
saktiworkstation <saktiworkstation@gmail.com>
2024-06-25 22:03:28 +0700

    mencoba membuat permission manage

---
+++ resources/views/permission-manage/create.blade.php
@@ -0,0 +1,61 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Assign Role To User') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    <section>
+                        <header>
+                            <h2 class="text-lg font-medium text-gray-900">
+                                {{ __('Assign Role') }}
+                            </h2>
+
+                            <p class="mt-1 text-sm text-gray-600">
+                                {{ __('Added Role To User Here...') }}
+                            </p>
+                        </header>
+
+                        <form method="POST" action="{{ route('role-manage.store') }}" class="mt-6 space-y-6">
+                            @csrf
+                            <div>
+                                <x-input-label for="user_id" :value="__('User')" />
+                                <x-select-input id="user_id" name="user_id" required>
+                                    <option value="">Choose User</option>
+                                    @foreach ($users as $data)
+                                        <option value="{{ $data->id }}"
+                                            {{ old('user_id') == $data->id ? 'selected' : '' }}>
+                                            {{ $data->name }}
+                                        </option>
+                                    @endforeach
+                                </x-select-input>
+                                <x-input-error class="mt-2" :messages="$errors->get('user_id')" />
+                            </div>
+                            <div>
+                                <x-input-label for="role_id" :value="__('Role')" />
+                                <x-select-input id="role_id" name="role_id" required>
+                                    <option value="">Choose Role</option>
+                                    @foreach ($roles as $data)
+                                        <option value="{{ $data->id }}"
+                                            {{ old('role_id') == $data->id ? 'selected' : '' }}>
+                                            {{ $data->name }}
+                                        </option>
+                                    @endforeach
+                                </x-select-input>
+                                <x-input-error class="mt-2" :messages="$errors->get('role_id')" />
+                            </div>
+
+                            <div class="flex items-center gap-4">
+                                <x-primary-button>{{ __('Save') }}</x-primary-button>
+                            </div>
+                        </form>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
---
+++ resources/views/permission-manage/index.blade.php
@@ -0,0 +1,58 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Role Manage') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    <x-link-primary-button :url="route('role-manage.create')">{{ __('Assign Role to USer') }}</x-link-primary-button>
+                </div>
+                @if (session()->has('success'))
+                    <x-success-alert>
+                        {{ session('success') }}
+                    </x-success-alert>
+                @endif
+            </div>
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-4xl">
+                    <h4 class="pb-3 font-semibold text-xl text-gray-800 leading-tight">
+                        {{ __('role-manage Data') }}
+                    </h4>
+                    <table class="border-collapse border-slate-500">
+                        <thead class="bg-gray-800 text-white">
+                            <tr>
+                                <th class="px-6 py-2 border border-slate-600">User Name</th>
+                                <th class="px-6 py-2 border border-slate-600">User Role</th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-gray-500">
+                            @foreach ($datas as $data)
+                                <tr class="text-white">
+                                    <td class="px-6 py-2 border border-slate-700">{{ $data->name }}</td>
+                                    <td class="px-6 py-2 border border-slate-700">
+                                        @foreach ($data->roles as $role)
+                                            <form action="/role-manage/{{ $data->id }}/delete/{{ $role->name }}"
+                                                method="post" class="d-inline">
+                                                @method('delete')
+                                                @csrf
+                                                <button
+                                                    class="inline-flex items-center px-3 py-1 bg-gray-800 border border-transparent rounded-md font-medium text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                                                    onclick="return confirm('Are you sure want to delete {{ $role->name }}?')">{{ $role->name }}</span>
+                                                </button>
+                                            </form>
+                                        @endforeach
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
